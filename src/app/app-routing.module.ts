cc0f5aaeda7f2c680cfd178a0dce09f064b5b5c9
Yehoyakim88 <yehoyakim@mail.de>
2023-03-11 12:07:01 +0100

    added rezepte.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RezepteComponent } from './rezepte/rezepte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'rezepte', component: RezepteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/rezepte/rezepte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rezepte',
+  templateUrl: './rezepte.component.html',
+  styleUrls: ['./rezepte.component.scss']
+})
+export class RezepteComponent {
+
+}
